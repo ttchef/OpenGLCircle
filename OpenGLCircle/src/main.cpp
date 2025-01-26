345cc6d9239ef2f65035dfd69d49dd96d2850634
ttchef <ruro0.edit@gmail.com>
2025-01-26 14:13:03 +0100

    Add project files.

---
+++ OpenGLCircle/src/main.cpp
@@ -0,0 +1,99 @@
+
+
+#include <iostream>
+#include <filesystem>
+
+#include "glad\glad.h"
+#include "GLFW\glfw3.h"
+
+namespace conf
+{
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		std::cerr << "[ERROR] Failed initializing GLFW!\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(conf::WIDTH, conf::HEIGHT, "Mefs Kreis", NULL, NULL);
+	if (!window)
+	{
+		std::cerr << "[ERROR] Failed Creating a Window!\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "[ERROR] Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	// GLFW Settings
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// OPENGL Settings
+	glViewport(0, 0, conf::WIDTH, conf::HEIGHT);
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f
+	};
+
+	unsigned int indices[]{
+		0, 1, 2,
+		1, 2, 3
+	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
