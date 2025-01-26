ef7bcda1776d615660933ba9b36c0d2ca594d65b
ttchef <ruro0.edit@gmail.com>
2025-01-26 14:35:24 +0100

    added shader

--- OpenGLCircle/src/main.cpp
+++ OpenGLCircle/src/main.cpp
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <filesystem>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 #include "glad\glad.h"
 #include "GLFW\glfw3.h"
@@ -79,14 +82,94 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	std::string vertexStr;
+	std::string fragmentStr;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try
+	{
+		vShaderFile.open("src/Shader.vert");
+		fShaderFile.open("src/Shader.frag");
+		std::stringstream vShaderStream, fShaderStream;
+
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+
+		vShaderFile.close();
+		fShaderFile.close();
+
+		vertexStr = vShaderStream.str();
+		fragmentStr = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cerr << "[ERROR] Failed Reading Shader File!\n";
+	}
+
+	const char* vShaderCode = vertexStr.c_str();
+	const char* fShaderCode = fragmentStr.c_str();
+
+	unsigned int vertexShader, fragmentShader;
+	int success;
+	char infoLog[512];
+	
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vShaderCode, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "[ERROR] Failed Compiling Vertex Shader!\n" << infoLog << std::endl;;
+	}
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cerr << "[ERROR] Failed Compiling Fragment Shader!\n" << infoLog << std::endl;;
+	}
+
+	unsigned int shader;
+	shader = glCreateProgram();
+	glAttachShader(shader, vertexShader);
+	glAttachShader(shader, fragmentShader);
+	glLinkProgram(shader);
+
+	glGetProgramiv(shader, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shader, 512, NULL, infoLog);
+		std::cerr << "[ERROR] Failed Linking Shader Program!\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
